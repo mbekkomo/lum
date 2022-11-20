fe843cd1603c02c0d86f97765df78a7df46005aa
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-11-20 08:28:22 +0000

    idk

--- lum.lua
+++ lum.lua
@@ -1,23 +1,49 @@
-local L = require "Lust"
-
 --- @class lum
 --[[
 The lum module
 ]]
 local lum = {}
 
-local execute = os.execute
+local os_execute = os.execute
+local io_popen = io.popen
+
+-- TODO:
+--  Implement `gum spin` with function instead command.
 
 --- @param prompt? string The prompt string
---- @return boolean # The return of confirmation
+--- @return boolean confirmation The return of confirmation
 --[[
 <s>
 Return `true` if selection is affirmative, otherwise `false` if selection is negative.
 ]]
 function lum.confirm(prompt)
-	local _,_,code = execute("gum confirm "..(prompt or ""))
-
+	local _,_,code = os_execute("gum confirm "..(prompt or ""))
 	return code < 1
+end
+
+--- @return string text The text
+--[[
+<s>
+Start a multi-line prompt
+]]
+function lum.write()
+	local gum = io_popen "gum write"
+	local data = gum:read "a":gsub("\n$","")
+	gum:close()
+	return data
+end
+
+--- @param path? string The path to select the file
+--- @return string file The file that user selected
+--[[
+<s>
+Start a prompt to select file in `path`
+]]
+function lum.file(path)
+	local gum = io.popen("gum file "..(path or "."))
+	local data = gum:read "a":gsub("\n$","")
+	gum:close()
+	return data
 end
 
 return lum
