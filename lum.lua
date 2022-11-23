a19f9a991a3557c73d506349f1a19a81ace45517
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-11-23 12:57:33 +0000

    some update

--- lum.lua
+++ lum.lua
@@ -1,14 +1,82 @@
+--[[
+MIT License
+
+Copyright (c) 2022 kooshie
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+
+--[[
+ TODO:
+ - Implement `gum spin` with function
+ - Add support for Windows
+]]
+
+--[[ Start ]]--
+
 --- @class lum
 --[[
 The lum module
 ]]
 local lum = {}
 
-local os_execute = os.execute
-local io_popen = io.popen
+--[[ Misc. ]]--
 
--- TODO:
---  Implement `gum spin` with function instead command.
+local os_execute = os.execute
+local io_popen,io_open = io.popen,io.open
+local table_insert,table_concat = table.insert,table.concat
+
+local toption = {
+	join = {
+		horizontal = "--horizontal",
+		vertical = "--vertical",
+		align = "--align"
+	}
+}
+
+local function table_copy(orig)
+	-- Src: http://lua-users.org/wiki/CopyTable
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		copy = {}
+		for orig_key, orig_value in pairs(orig) do
+			copy[orig_key] = orig_value
+		end
+	else -- number, string, boolean, etc
+		copy = orig
+	end
+	return copy
+end
+
+local function iswindows()
+	return type(package) == 'table' and type(package.config) == 'string' and package.config:sub(1,1) == '\\'
+end
+
+--[[ API ]]--
+
+--- @type boolean
+lum._winsupport = false
+
+if iswindows() and not lum._winsupport then
+	error "Lum only works with POSIX shell (`sh`), especially Linux. But you can enable Windows support by changing `lum._winsupport` to `true`."
+end
 
 --- @param prompt? string The prompt string
 --- @return boolean confirmation The return of confirmation
@@ -40,7 +108,42 @@ end
 Start a prompt to select file in `path`
 ]]
 function lum.file(path)
-	local gum = io.popen("gum file "..(path or "."))
+	local gum = io_popen("gum file "..(path or "."))
+	local data = gum:read "a":gsub("\n$","")
+	gum:close()
+	return data
+end
+
+function lum.join(...)
+	local option = {
+		horizontal = false,
+		vertical = false,
+		align = "left"
+	}
+
+	local vararg = {...}
+	if type(vararg[#vararg]) == "table" and next(vararg[#vararg]) then
+		option = table_copy(vararg[#vararg])
+		vararg[#vararg] = nil
+	elseif type(vararg[#vararg]) == "table" and not next(vararg[#vararg]) then
+		vararg[#vararg] = nil
+	end
+	local cmd = {}
+	cmd[1] = "gum join"
+	for o_name,o_val in pairs(option) do
+		if type(o_val) == "boolean" then
+			table_insert(cmd,o_val and toption.join[o_name] or "")
+		else
+			table_insert(cmd,toption.join[o_name].."='"..tostring(o_val).."'")
+		end
+	end
+
+	local buff = {}
+	for _,v in pairs(vararg) do
+		table_insert(buff,"'"..tostring(v).."'")
+	end
+
+	local gum = io_popen(table_concat(cmd," ").." "..table_concat(buff," "))
 	local data = gum:read "a":gsub("\n$","")
 	gum:close()
 	return data
