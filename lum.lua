056b0aeffef8572a122012a33763d12519ed4d87
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-11-25 10:35:56 +0800

    clean up

--- lum.lua
+++ lum.lua
@@ -39,8 +39,8 @@ local lum = {}
 --[[ Misc. ]]--
 
 local os_execute = os.execute
-local io_popen,io_open = io.popen,io.open
-local table_insert,table_concat = table.insert,table.concat
+local io_popen, io_open = io.popen, io.open
+local table_insert, table_concat = table.insert, table.concat
 
 local gum_option = {
 	join = {
@@ -71,26 +71,26 @@ local function table_copy(orig)
 end
 
 local function iswindows()
-	return type(package) == 'table' and type(package.config) == 'string' and package.config:sub(1,1) == '\\'
+	return type(package) == 'table' and type(package.config) == 'string' and package.config:sub(1, 1) == '\\'
 end
 
-local function cmd_handle(fn_name,cmd,option)
+local function cmd_handle(fn_name, cmd, option)
 	local cmd_buff = {}
 	cmd_buff[1] = cmd
 
 	local fn_option = gum_option[fn_name]
-	for o_name,o_val in pairs(option) do
+	for o_name, o_val in pairs(option) do
 		if not fn_option[o_name] then
-			error("Invalid option: "..o_name)
+			error("Invalid option: " .. o_name)
 		end
 
 		if type(o_val) == "boolean" then
-			table_insert(cmd_buff,o_val and fn_option[o_name] or "")
+			table_insert(cmd_buff, o_val and fn_option[o_name] or "")
 		else
-			table_insert(cmd_buff,fn_option[o_name].."='"..tostring(o_val).."'")
+			table_insert(cmd_buff, fn_option[o_name] .. "='" .. tostring(o_val) .. "'")
 		end
 	end
-	return cmd_buff
+	return table_concat(cmd_buff, " ")
 end
 
 --[[ API ]]--
@@ -102,6 +102,8 @@ if iswindows() and not lum._winsupport then
 	error "Lum only works with POSIX shell (`sh`), especially Linux. But you can enable Windows support by changing `lum._winsupport` to `true`."
 end
 
+
+
 --- @class confirm_option
 --- @field affirmative string The text for affirmative confirmation
 --- @field negative string The text for negative confirmation
@@ -109,23 +111,25 @@ end
 
 --- @param prompt? string The prompt string
 --- @param option? confirm_option The option for the function
--- @return boolean confirmation The return of confirmation
+--- @return boolean confirmation The return of confirmation
 --[[
 <s>
 Return `true` if selection is affirmative, otherwise `false` if selection is negative.
 ]]
-function lum.confirm(prompt,option)
-	local option = option or {
+function lum.confirm(prompt, option)
+	option = option or {
 		affirmative = "Yes",
 		negative = "No",
 		timeout = 0
 	}
 
-	local cmd = cmd_handle("confirm","gum confirm",option)
+	local cmd = cmd_handle("confirm", "gum confirm", option)
 
-	local _,_,code = os_execute(table_concat(cmd," ")..(prompt and " "..prompt or ""))
+	local _, _, code = os_execute(cmd .. (prompt and " " .. prompt or ""))
 	return code < 1
 end
+
+
 
 --- @return string text The text
 --[[
@@ -134,10 +138,12 @@ Start a multi-line prompt
 ]]
 function lum.write()
 	local gum = io_popen "gum write"
-	local data = gum:read "a":gsub("\n$","")
+	local data = gum:read "a":gsub("\n$", "")
 	gum:close()
 	return data
 end
+
+
 
 --- @param path? string The path to select the file
 --- @return string file The file that user selected
@@ -146,11 +152,13 @@ end
 Start a prompt to select file in `path`
 ]]
 function lum.file(path)
-	local gum = io_popen("gum file "..(path or "."))
-	local data = gum:read "a":gsub("\n$","")
+	local gum = io_popen("gum file " .. (path or "."))
+	local data = gum:read "a":gsub("\n$", "")
 	gum:close()
 	return data
 end
+
+
 
 --- @alias align
 ---| '"left"'
@@ -165,9 +173,9 @@ end
 --- @field vertical boolean Make joins vertical
 --- @field align align Align the joins
 
---- @param ... any Any value to join
+--- @param  ... any Any value to join
 --- @return string joined_text The joined value into string
---- @overload fun(...: any, option?: join_option)
+--- @overload fun( ...: any, option?: join_option)
 --[[
 <s>
 Join (or concatenate) values, used to join Gum's styled texts.
@@ -186,15 +194,15 @@ function lum.join(...)
 	elseif type(vararg[#vararg]) == "table" and not next(vararg[#vararg]) then
 		vararg[#vararg] = nil
 	end
-	local cmd = cmd_handle("join","gum join",option)
+	local cmd = cmd_handle("join", "gum join", option)
 
 	local buff = {}
-	for _,v in pairs(vararg) do
-		table_insert(buff,"'"..tostring(v).."'")
+	for _, v in pairs(vararg) do
+		table_insert(buff, "'"  ..  tostring(v) .. "'")
 	end
 
-	local gum = io_popen(table_concat(cmd," ").." "..table_concat(buff," "))
-	local data = gum:read "a":gsub("\n$","")
+	local gum = io_popen(cmd .. " " .. table_concat(buff, " "))
+	local data = gum:read "a":gsub("\n$", "")
 	gum:close()
 	return data
 end
